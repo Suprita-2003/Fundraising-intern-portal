c180bcbaf3e24062ffba7e8bb711e2d60e1e881e
Suprita-2003 <sanadanisuprita@gmail.com>
2025-08-02 01:47:53 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+/// firebase-config.js
+const firebaseConfig = {
+    apiKey: "AIzaSyAMasAyxkB0oRmUj5FYTGZVrb70wEdHgBs",
+    authDomain: "fundraising-intern-portal.firebaseapp.com",
+    databaseURL: "https://fundraising-intern-portal-default-rtdb.firebaseio.com",
+    projectId: "fundraising-intern-portal",
+    storageBucket: "fundraising-intern-portal.appspot.com",
+    messagingSenderId: "157339832650",
+    appId: "1:157339832650:web:7304d9275516f73c4031b3"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
